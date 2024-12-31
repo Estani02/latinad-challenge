d6863ce34a36518d513319113abed2c87c7fb599
Cuenta Personal <estaniolmedo@gmail.com>
2024-12-30 21:06:23 -0300

    Add index files for specific components to streamline exports

---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export * as Specific from './specific';
---
+++ src/components/specific/index.ts
@@ -0,0 +1,9 @@
+export * as List from './list';
+
+export * as Card from './card';
+
+export * as AutoComplete from './auto-complete';
+
+export * as From from './form';
+
+export * as DataPicker from './data-picker';
