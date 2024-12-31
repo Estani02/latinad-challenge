76d5f2d26973ba74d32deac7af481cefd91e3528
Cuenta Personal <estaniolmedo@gmail.com>
2024-12-31 17:00:16 -0300

    Add calculateDaysBetweenDates utility function for date difference calculation

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import {LocationType, SizeType} from './types';
+import {FormatDate, LocationType, SizeType} from './types';
 
 export const translateSizeType = (sizeType: SizeType): string => {
   switch (sizeType) {
@@ -36,3 +36,25 @@ export const formatMillisecondsToMinutesAndSeconds = (ms: number) => {
 
   return `${minutes}:${parseInt(seconds) < 10 ? '0' : ''}${seconds}`;
 };
+
+export function calculateDaysBetweenDates(dates: FormatDate[]): number {
+  if (!Array.isArray(dates) || dates.length !== 2) {
+    throw new Error("El array debe contener exactamente dos fechas en formato 'YYYY-MM-DD'.");
+  }
+
+  const [startDateString, endDateString] = dates;
+
+  if (!startDateString || !endDateString) {
+    throw new Error('Date strings cannot be null');
+  }
+  const startDate = new Date(startDateString);
+  const endDate = new Date(endDateString);
+
+  if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+    throw new Error('Date is invalid');
+  }
+
+  const differenceInMilliseconds = endDate.getTime() - startDate.getTime();
+
+  return differenceInMilliseconds / (1000 * 60 * 60 * 24); // Convertir a días
+}
