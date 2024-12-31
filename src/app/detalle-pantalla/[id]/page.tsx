c7789ab0c2bba90856946f9e2c3b45247b7649cd
Cuenta Personal <estaniolmedo@gmail.com>
2024-12-31 01:16:04 -0300

    Enhance ScreenDetail component with loading skeletons and update Header link to home

--- src/app/detalle-pantalla/[id]/page.tsx
+++ src/app/detalle-pantalla/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import {useParams} from 'next/navigation';
 import Image from 'next/image';
-import {Card, Descriptions} from 'antd';
+import {Card, Descriptions, Skeleton} from 'antd';
 import Link from 'next/link';
 
 import {selectCampaignById} from '@/features/campaignSlice';
@@ -17,7 +17,23 @@ export default function ScreenDetail() {
   const campaign = useAppSelector((state) => selectCampaignById(state, Number(id)));
 
   if (!campaign) {
-    return <div>Loading...</div>;
+    return (
+      <div className="mt-4 flex h-full w-full items-center justify-center px-[10px] py-6">
+        <div className="flex w-full max-w-[1000px] flex-col gap-6 rounded-2xl bg-white">
+          <div className="relative h-[300px] w-full overflow-hidden rounded-t-xl">
+            <Skeleton.Image active className="!h-full !max-h-[700px] !w-full !max-w-[1000px]" />
+          </div>
+          <div className="flex flex-col gap-6 p-6">
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+          </div>
+        </div>
+      </div>
+    );
   }
   const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${campaign.latitude},${campaign.longitude}`;
 
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <div className="sticky top-0 z-50 p-[10px]">
       <header className="flex w-full items-center rounded-2xl bg-white px-5 py-5">
-        <Link passHref href="https://latinad.com/" target="_blank">
+        <Link passHref href="/">
           <Image alt="logo" height={35} src="/logo.svg" width={130} />
         </Link>
       </header>
